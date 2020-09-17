54d5affe261494f2a288263e0dd8e7f286dd38b0
Nitukumari123 <47345529+Nitukumari123@users.noreply.github.com>
2020-09-17 13:41:23 +0530

    Add files via upload
    Working Google API

---
+++ src/main/java/com/telusko/demotelusko/DemoteluskoApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.demotelusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoteluskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoteluskoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telusko/demotelusko/DemoteluskoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.demotelusko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoteluskoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
